31c4e5d6788eb25bff14e9b665958c5ca312ac25
Glenn Inn <glenninn@yahoo.com>
2018-04-29 22:02:51 -0700

    Initial Checkin

---
+++ README.md
@@ -0,0 +1,58 @@
+
+
+# Test Application to Generate OutDial from BlueJeans
+
+This test application is written to demonstrate and test how to place an outdial from a BlueJeans meeting.  The outdial may be done to ***either*** a **PSTN** or a **SIP** endpoint.
+
+## Getting Started
+The application is written in NodeJS.  You will need to have installed `NodeJS` as well as its package manager application, `npm`.
+
+
+
+1. Download the application from github.
+2. Initialize the app environment by running `npm install`
+
+
+# Running a session
+This application requires mandatory command line options:
+
+- numeric mtg id This is the meeting ID for the BlueJeans meeting where the outdialed participant will join
+- p / s : use the letter (lower case) p for PSTN outdial, or s for SIP callout
+- dial# : the PSTN phone number (USA only) or the SIP uri
+
+
+This is the CLI syntax to run a dialout session.  This example will place a PSTN phone call to 415-990-8751
+
+		node dialout.js 779192087 p 4159908751
+
+
+
+
+
+
+# Sample output from a PSTN command line session
+
+    
+    C:\users\glinn\Documents\node\testdialout> node dialout.js 779192087 p 4159908751
+    ------------------------------------------------------
+    testdialout
+       Simple test application to cause BlueJeans to do a PSTN or SIP
+       call-out to a meeting participant
+    ------------------------------------------------------
+    
+    
+    Acquired Meeting Access Token for mtg: 779192087
+     token: b509ebcaa17c415eab3eb3748f616471@z8
+    ---> Setting userId: 1442589
+    
+    Obtained pairing codes
+      connectionGuid=   connguid:4ee695fc-48b6-41cd-a356-78235449697e
+      seamEndpointGuid= seamguid:f5185206-50f2-4e44-bf2f-b8a4ad4df6bf
+    
+    *******************************************
+    * Making PSTN outdial
+    *******************************************
+    
+    Success! Kicked-off --PSTN-- outdial to: 4159908751
+    
+    C:\Users\glinn\Documents\node\testdialout>`
---
+++ dialout.js
@@ -0,0 +1,139 @@
+var auth = require("./auth");
+
+
+var userId = "1442589";
+
+console.log("------------------------------------------------------");
+console.log("testdialout");
+console.log("   Simple test application to cause BlueJeans to do a PSTN or SIP");
+console.log("   call-out to a meeting participant");
+console.log("------------------------------------------------------\n");
+
+if (process.argv.length != 5) {
+	console.log("\nUsage: node dialout.js  <numeric mtg id> <p:pstn or s:sip> <dial#>");
+	console.log("  Generate pairing code and outdial from the specified BlueJeans meeting");
+	console.log("Where:");
+	console.log("   <numeric mtg id> : the BlueJeans Meeting to join");
+	console.log("   s/p :     Join via outcall to PSTN or SIP" );
+	console.log("   <dial#> : The PSTN 10-digit #, or a SIP uri");
+	process.exit();
+}
+
+var numericMtgId = process.argv[2];
+var isPSTN = (process.argv[3]=='p');
+var dialTo = process.argv[4];
+
+
+var getMtgAccessTokenRec =
+{
+  grant_type : "meeting_passcode",
+  meetingNumericId : "",
+  meetingPasscode : ""
+};
+
+var pairCodeRec = {
+  endpointName : "call iphoasdfasne xx2",
+  endpointType : "PSTN",
+  userId : 0,
+  languageCode : "en",
+  capabilities : [
+    "AUDIO"
+  ]
+};
+
+var dialoutRecPSTN =
+{
+  connectionGuid : "",
+  countryCode : 1,
+  pairedParticipantGuid : "",
+  phoneNumber : ""
+};
+
+var dialoutRecSIP =
+{
+  uri : ""
+}
+
+
+function makePSTNCall(pcResults){
+	console.log("\n*******************************************");
+	console.log(  "* Making PSTN outdial")
+	console.log(  "*******************************************");
+
+	dialoutRecPSTN.connectionGuid = pcResults.connectionGuid;
+	dialoutRecPSTN.pairedParticipantGuid = pcResults.seamEndpointGuid;		
+	dialoutRecPSTN.phoneNumber = dialTo;
+	
+	auth.post(apihost,makeOutdialPSTN,dialoutRecPSTN).then( (doResults)=>{
+		console.log("\nSuccess! Kicked-off --PSTN-- outdial to: " + dialoutRecPSTN.phoneNumber);
+		
+	}, (doErrors) => {
+		console.log("\n?Error:  unable to make dialout: " + JSON.stringify(doErrors,null,2) );
+	});
+}
+
+
+function makeSIPCall(pcResults){
+	console.log("\n*******************************************");
+	console.log(  "* Making SIP outdial")
+	console.log(  "*******************************************");
+
+	//dialoutRecPSTN.connectionGuid = pcResults.connectionGuid;
+	//dialoutRecPSTN.pairedParticipantGuid = pcResults.seamEndpointGuid;		
+	dialoutRecSIP.uri = dialTo;
+	
+	auth.post(apihost,makeOutdialPSTN,dialoutRecSIP).then( (doResults)=>{
+		console.log("\nSuccess! Kicked-off --SIP-- outdial to: " + dialoutRecPSTN.phoneNumber);
+		
+	}, (doErrors) => {
+		console.log("\n?Error:  unable to make dialout: " + JSON.stringify(doErrors,null,2) );
+	});
+}
+
+
+function setAPIfields(){
+	getPairingCodePSTN=getPairingCodePSTN.replace("{user_id}",userId);
+	getPairingCodePSTN=getPairingCodePSTN.replace("{numeric_meeting_id}",numericMtgId);
+	makeOutdialPSTN=makeOutdialPSTN.replace("{user_id}",userId);
+	makeOutdialPSTN=makeOutdialPSTN.replace("{numeric_meeting_id}",numericMtgId);
+}
+
+
+var apihost = "api.bluejeans.com";
+var getMtgAccessToken = "/oauth2/token";
+var getPairingCodePSTN = "/v1/user/{user_id}/live_meetings/{numeric_meeting_id}/pairing_code/PSTN";
+var makeOutdialPSTN = "/v1/user/{user_id}/live_meetings/{numeric_meeting_id}/dialout/pstn";
+
+//setup for getting a mtg access token
+getMtgAccessTokenRec.meetingNumericId = numericMtgId;
+
+auth.post(apihost,getMtgAccessToken,getMtgAccessTokenRec).then( (results)=>{
+	console.log("\nAcquired Meeting Access Token for mtg: " + numericMtgId + "\n token: " + results.access_token);
+	
+	auth.authorize(  results.access_token );   // encodeURI(results.access_token) );
+	userId = results.scope.meeting.leaderId;
+	console.log("---> Setting userId: " + userId);
+	setAPIfields();
+	
+	auth.post(apihost,getPairingCodePSTN,pairCodeRec).then( (pcResults)=>{
+		console.log("\nObtained pairing codes");
+		console.log("  connectionGuid=   " + pcResults.connectionGuid);
+		console.log("  seamEndpointGuid= " + pcResults.seamEndpointGuid);
+		
+		if(isPSTN){
+			makePSTNCall(pcResults);
+		} else {
+			makeSIPCall(pcResults);
+		}
+		
+	}, (pcErrors)=> {
+		console.log("\n?Error:  unable to get pairing code: " + JSON.stringify(pcErrors,null,2) );
+		
+	});
+	
+}, (errors) => {
+		console.log("\n?Error:  unable to get meeting access token: " + JSON.stringify(errors,null,2) );
+	
+});
+
+
